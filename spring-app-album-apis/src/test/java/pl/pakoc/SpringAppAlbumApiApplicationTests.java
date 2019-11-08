74478314afa1620a60e32ad9930db3c643c7f49b
Paweł Kociuga <pawel.kociuga@accenture.com>
2019-11-08 13:06:59 +0100

    change name for class test

--- spring-app-album-apis/src/test/java/pl/pakoc/SpringAppAlbumApiApplicationTests.java
+++ spring-app-album-apis/src/test/java/pl/pakoc/SpringAppAlbumApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.appsdeveloperblog.photoapp.api.albums.PhotoAppApiAlbums;
+package pl.pakoc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PhotoAppApiAlbumsApplicationTests {
+public class SpringAppAlbumApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
